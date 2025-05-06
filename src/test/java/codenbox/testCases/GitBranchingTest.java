23d6b55a391a8281d293760d5a4ecb319c9b5eae
nisheshpradhananga <nishesh166@gmail.com>
2025-05-06 12:47:46 -0500

    added new class name GitBranchingtest

---
+++ src/test/java/codenbox/testCases/GitBranchingTest.java
@@ -0,0 +1,12 @@
+package codenbox.testCases;
+
+import org.testng.annotations.Test;
+
+public class GitBranchingTest {
+	@Test
+	public void demo() {
+		System.out.println("This is demo method for branching test");
+	}
+	
+
+}
